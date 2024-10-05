0af3f0ad8cc27d5870165d357744020d5cb346a4
Max-Chu1125 <williechu1125@gmail.com>
2024-10-05 20:32:13 +0800

    Add Transition Effect

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -4,7 +4,7 @@ import {motion} from 'framer-motion'
 const TransitionEffect = () => {
   return (
     <>
-        {/* <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
+        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
         initial={{x:"100%", width:"100%"}}
         animate={{x:"0%", width:"0%"}}
         exit={{x:["0%", "100%"], width:["0%", "100%"]}}
@@ -21,7 +21,7 @@ const TransitionEffect = () => {
         animate={{x:"0%", width:"0%"}}
         exit={{x:["0%", "100%"], width:["0%", "100%"]}}
         transition={{delay:0.4, duration:0.8, ease:"easeInOut"}}
-         /> */}
+         />
     </>
   )
 }
